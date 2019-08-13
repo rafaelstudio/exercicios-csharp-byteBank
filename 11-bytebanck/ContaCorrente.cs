5f95e5b225c219c082cee4f9da41f363be6fdce0
Rafael Nunes dos Santos <studiosafeway@gmail.com>
2019-08-13 15:09:46 -0300

    first commit

---
+++ 11-bytebanck/ContaCorrente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _11_bytebanck
+{
+    public  class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int conta;
+        public double saldo = 100;
+
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(this.saldo < valor)
+            {
+                Console.WriteLine("Saldo Insulficiente !!!");
+                return false;
+
+            }
+
+            this.saldo -= valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if(this.saldo < valor)
+            {
+                Console.WriteLine("Transferencia não realizada, Saldo insulficiente");
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
+    }
+
+    
+}
---
+++ 11-bytebanck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _11_bytebanck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente primeiraConta = new ContaCorrente();
+            
+             primeiraConta.saldo = 200;
+            Console.WriteLine(primeiraConta.saldo);
+            primeiraConta.saldo += 100;
+            Console.WriteLine(primeiraConta.saldo);
+
+            ContaCorrente segundaConta = new ContaCorrente();
+            segundaConta.saldo = 50;
+
+            Console.WriteLine("A primeira conta tem: " + primeiraConta.saldo);
+            Console.WriteLine("A segunda conta tem: " + segundaConta.saldo);
+
+            primeiraConta.Depositar(150);
+            Console.WriteLine("Saldo apos deposito : "+primeiraConta.saldo);
+            primeiraConta.Transferir(200, segundaConta);
+            Console.WriteLine("Saldo primeiraConta  apos Tranferencia  : " + primeiraConta.saldo);
+            Console.WriteLine("Saldo segundaConta apos Tranferencia    : " + segundaConta.saldo);
+            segundaConta.Sacar(50.50);
+            Console.WriteLine("Saldoas segundaConta apos saque : "+segundaConta.saldo);
+
+            Console.ReadLine();
+
+           
+        }
+    }
+}
